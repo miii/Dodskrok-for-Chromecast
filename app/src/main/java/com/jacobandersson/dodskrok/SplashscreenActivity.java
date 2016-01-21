1e1172177148805d9597b06b31d41ec5e46c0476
Jacob Andersson <jacob.m.andersson@gmail.com>
2016-01-21 20:50:55 +0100

    - Splashscreen added

---
+++ app/src/main/java/com/jacobandersson/dodskrok/SplashscreenActivity.java
@@ -0,0 +1,26 @@
+package com.jacobandersson.dodskrok;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashscreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+
+        GoogleCast.getInstance(this);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashscreenActivity.this, PlayingActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1500);
+    }
+}
